24ec299230422199b78fe25efef90f769be304e6
Holly Chaffee <saltdc95@gmail.com>
2022-04-10 05:32:02 -0400

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+   // Calculator which performs basic operations addition, subtraction, multiplication, division, and modulus operators depending on the user input
+    public Calculator() {
+    }
+    public int add(int a, int b) {
+      return a + b;
+    }
+    public int subtract(int a, int b) {
+      return a - b;
+    }
+    public int multiply(int a, int b) {
+      return a * b;
+    }
+    public int divide(int a, int b) {
+      return a / b;
+    }
+    public int modulo(int a, int b) {
+      return a % b;
+    }
+    public static void main(String args[]) {
+      Calculator myCalculator = new Calculator();
+
+      System.out.println(myCalculator.add(5, 7));
+      System.out.println(myCalculator.subtract(45, 11));
+    }
+  }
